57d9d9c79da5987f44371ebe3f2d7c48142c65e6
Kevin Small <kevsm@proton.me>
2024-06-26 16:28:57 -0700

    Testing array push loop, logs properly to console

--- src/Visualizer/Visualizer.js
+++ src/Visualizer/Visualizer.js
@@ -1,13 +1,24 @@
 function Visualizer() {
+  let array = [];
+  const arraypush = (array) => {
+    for (let i = 0; i < 100; i++) {
+      array.push('test');
+    }
+    return array;
+  };
+  console.log(arraypush(array));
+
   const test = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 
   return (
     <>
       <div className="array-container">
         {test.map((value, index) => (
-          <div key={index} className="array-block" style={{height: `${value}px`}}>
-            {/* <h1>{value}</h1> */}
-          </div>
+          <div
+            key={index}
+            className="array-block"
+            style={{ height: `${value}px` }}
+          ></div>
         ))}
       </div>
     </>
