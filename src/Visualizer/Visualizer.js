612dc0453c0c7eaf33edceea3bf6c7d5f4006047
Kevin Small <kevsm@proton.me>
2024-06-27 16:26:44 -0700

    added bubble sort function test

--- src/Visualizer/Visualizer.js
+++ src/Visualizer/Visualizer.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import bubbleSort from '../utils/bubbleSort';
 
 const Visualizer = () => {
   const [array, setArray] = useState([]);
@@ -7,7 +8,7 @@ const Visualizer = () => {
     return Math.floor(Math.random() * (max - min)) + min;
   };
 
-  const arrayPush = () => {
+  const generateArray = () => {
     const newArray = [];
     for (let i = 0; i < 75; i++) {
       newArray.push(arrayRandom(1, 800));
@@ -16,7 +17,7 @@ const Visualizer = () => {
   };
 
   useEffect(() => {
-    arrayPush();
+    generateArray();
   }, []);
 
   return (
@@ -32,7 +33,8 @@ const Visualizer = () => {
           ))}
         </div>
       </div>
-        <button onClick={arrayPush}>Generate</button>
+      <button onClick={generateArray}>Generate</button>
+      <button onClick={generateArray}>Bubblesort console test</button>
     </>
   );
 };
