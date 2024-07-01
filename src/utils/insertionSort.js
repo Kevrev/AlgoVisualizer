c0860a127d2a9be54d526cced48f91a69d622dc0
Kevin Small <kevsm@proton.me>
2024-07-01 14:53:41 -0700

    Added insrtion sort

--- src/utils/insertionSort.js
+++ src/utils/insertionSort.js
@@ -1,5 +1,15 @@
 const insertionSort = array => {
-
+  let n = array.length;
+  for (let i = 1; i < n; i++) {
+    let current = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+  array[j + 1] = current;
+  }
+  return array;
 }
 
 export default insertionSort;
