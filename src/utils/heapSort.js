6218b4a51cecbb943359783cc4d9771df672f2e8
Kevin Small <kevsm@proton.me>
2024-07-02 16:33:27 -0700

    building heap sort

--- src/utils/heapSort.js
+++ src/utils/heapSort.js
@@ -1,5 +1,10 @@
 const heapSort = array => {
-
+  const buildMaxHeap = array => {
+    let n = array.length;
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+      heapify(array, n, i);
+    }
+  }
 }
 
 export default heapSort;
