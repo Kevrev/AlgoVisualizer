d22f349e2c74e18194802497434fb0f814ff3470
Kevin Small <kevsm@proton.me>
2024-07-02 14:36:06 -0700

    Added merge portion of merge sort

--- src/utils/mergeSort.js
+++ src/utils/mergeSort.js
@@ -2,4 +2,21 @@ const mergeSort = array => {
 
 }
 
+const merge = (left, right) => {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
 export default mergeSort;
