0a0ed16b27357e86730385256cecfb84f3cd5ad5
Kevin Small <kevsm@proton.me>
2024-07-08 16:19:02 -0700

    applied same method to selection sort, potentially needs work but animation functions

--- src/utils/bubbleSort.js
+++ src/utils/bubbleSort.js
@@ -1,3 +1,19 @@
+// const bubbleSort = (array) => {
+//   // implement break logic to stop unnecessary passes on sorted arrays
+//   let n = array.length
+//   for (let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//       if (array[j] > array[j + 1]) {
+//         [array[j], array[j + 1]] = 
+//         [array[j + 1], array[j]];
+//       }
+//     }
+//   }
+//  return array;
+// };
+
+// export default bubbleSort;
+
 const bubbleSort = async (array, setArray, setActiveIndex) => {
   // implement break logic to stop unnecessary passes on sorted arrays
   let n = array.length
--- src/utils/selectionSort.js
+++ src/utils/selectionSort.js
@@ -1,17 +1,42 @@
-const selectionSort = (array) => {
+// const selectionSort = (array) => {
+//   let n = array.length;
+//   for (let i = 0; i < n - 1; i++) {
+//     let minIndex = i;
+//     for (let j = i + 1; j < n; j++) {
+//       if (array[j] < array[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+//     if (minIndex != i) {
+//       [array[i], array[minIndex]] = [array[minIndex], array[i]];
+//     }
+//   }
+//   return array;
+// };
+
+// export default selectionSort;
+
+
+const selectionSort = async (array, setArray, setActiveIndex) => {
   let n = array.length;
   for (let i = 0; i < n - 1; i++) {
     let minIndex = i;
     for (let j = i + 1; j < n; j++) {
+
+      await new Promise(resolve => setTimeout(resolve, 3));
+      
       if (array[j] < array[minIndex]) {
         minIndex = j;
+        setActiveIndex(j);
       }
     }
     if (minIndex != i) {
       [array[i], array[minIndex]] = [array[minIndex], array[i]];
+      setArray([...array]);
+      setActiveIndex(i);
     }
   }
-  return array;
+  setActiveIndex(null);
 };
 
 export default selectionSort;
