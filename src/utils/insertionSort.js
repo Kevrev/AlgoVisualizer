253cb2bef9038831cfb859bf15d94aeb691b8aa4
Kevin Small <kevsm@proton.me>
2024-07-08 17:40:55 -0700

    Added animation for insertion sort

--- src/utils/insertionSort.js
+++ src/utils/insertionSort.js
@@ -1,15 +1,36 @@
-const insertionSort = array => {
+// const insertionSort = array => {
+//   let n = array.length;
+//   for (let i = 1; i < n; i++) {
+//     let current = array[i];
+//     let j = i - 1;
+//     while (j >= 0 && array[j] > current) {
+//       array[j + 1] = array[j];
+//       j--;
+//     }
+//   array[j + 1] = current;
+//   }
+//   return array;
+// }
+
+// export default insertionSort;
+
+const insertionSort = async (array, setArray, setActiveIndex) => {
   let n = array.length;
   for (let i = 1; i < n; i++) {
     let current = array[i];
     let j = i - 1;
+
+ await new Promise(resolve => setTimeout(resolve, 3));
+
     while (j >= 0 && array[j] > current) {
+      setActiveIndex(j);
       array[j + 1] = array[j];
+      setArray([...array]);
       j--;
     }
   array[j + 1] = current;
   }
-  return array;
+  setActiveIndex(null);
 }
 
 export default insertionSort;
